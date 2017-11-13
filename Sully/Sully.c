b5a315332f124493a002cd87225bc7f83fae8c8e
jcamhi <jeremshy@h3lp.fr>
2017-11-13 18:16:13 +0100

    Rainbow Dash is best pony !

--- Sully/Sully.c
+++ Sully/Sully.c
@@ -3,17 +3,20 @@
 #include <stdio.h>
 int main() {
 int X=5;
-char XC=X+48-1;
+int Y=1;
+char XC=X+48;
+Y == 1 ? Y = 0 : XC--;
+char YC=Y+48;
 char bufferc[20]="Sully_X.c";
 bufferc[6]=XC;
 int fd=open(bufferc,513,438);
-char *s="#include <fcntl.h>%c#include <stdlib.h>%c#include <stdio.h>%cint main() {%cint X=%c;%cchar XC=X+48-1;%cchar bufferc[20]=%cSully_X.c%c;%cbufferc[6]=XC;%cint fd=open(bufferc,513,438);%cchar *s=%c%s%c;%cdprintf(fd,s,10,10,10,10,XC,10,10,34,34,10,10,10,34,s,34,10,10,34,34,10,10,10,10,10,34,34,10,10,10);%cchar sysbuf[50]=%cclang -Wall -Wextra -Werror Sully_X.c -o Sully_X;%c;%csysbuf[34]=XC;%csysbuf[47]=XC;%csystem(sysbuf);%cif (X <= 0) {return (0);}%cchar sysbuf2[50]=%c./Sully_X%c;%csysbuf2[8]=XC;%csystem(sysbuf2);}%c";
-dprintf(fd,s,10,10,10,10,XC,10,10,34,34,10,10,10,34,s,34,10,10,34,34,10,10,10,10,10,34,34,10,10,10);
+char *s="#include <fcntl.h>%c#include <stdlib.h>%c#include <stdio.h>%cint main() {%cint X=%c;%cint Y=%c;%cchar XC=X+48;%cY == 1 ? Y = 0 : XC--;%cchar YC=Y+48;char bufferc[20]=%cSully_X.c%c;%cbufferc[6]=XC;%cint fd=open(bufferc,513,438);%cchar *s=%c%s%c;%cdprintf(fd,s,10,10,10,10,XC,10,YC,10,10,10,34,34,10,10,10,34,s,34,10,10,34,34,10,10,10,10,10,34,34,10,10,10);%cchar sysbuf[50]=%cclang -Wall -Wextra -Werror Sully_X.c -o Sully_X;%c;%csysbuf[34]=XC;%csysbuf[47]=XC;%csystem(sysbuf);%cif (X <= 1) {return (0);}%cchar sysbuf2[50]=%c./Sully_X%c;%csysbuf2[8]=XC;%csystem(sysbuf2);}%c";
+dprintf(fd,s,10,10,10,10,XC,10,YC,10,10,10,34,34,10,10,10,34,s,34,10,10,34,34,10,10,10,10,10,34,34,10,10,10);
 char sysbuf[50]="clang -Wall -Wextra -Werror Sully_X.c -o Sully_X;";
 sysbuf[34]=XC;
 sysbuf[47]=XC;
 system(sysbuf);
-if (X <= 0){return (0);}
+if (X <= 1){return (0);}
 char sysbuf2[50]="./Sully_X";
 sysbuf2[8]=XC;
 system(sysbuf2);}
